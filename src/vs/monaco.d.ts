33b5d5a50b4fec3c9d5a123d5764af5603e85487
Tiago Braga Fernandes <tiago.fernandes@outsystems.com>
2026-03-05 15:38:34 +0000

    Commit automatically generated monaco d.ts file

--- src/vs/monaco.d.ts
+++ src/vs/monaco.d.ts
@@ -7423,6 +7423,10 @@ declare namespace monaco.languages {
 		 * A command that should be run upon acceptance of this item.
 		 */
 		action?: Command;
+		/**
+		 * Custom icon to be used (instead of those ones that depend on the completionItem kind)
+		 */
+		customIcon?: HTMLElement;
 	}
 
 	export interface CompletionList {

3ba499aa8ced8f3b455886576725276bf6338eb6
Rui Neves <rui.neves@outsystems.com>
2026-04-30 15:17:57 +0100

    Prevent crash when firstChild is not a text node

--- src/vs/editor/browser/controller/mouseTarget.ts
+++ src/vs/editor/browser/controller/mouseTarget.ts
@@ -1172,8 +1172,13 @@ function shadowCaretRangeFromPoint(shadowRoot: ShadowRoot, x: number, y: number)
 		}
 
 		// Creates a range with the text node of the element and set the offset found
-		range.setStart(el.firstChild!, offset);
-		range.setEnd(el.firstChild!, offset);
+		if (el.firstChild!.nodeType === 3 /* TEXT_NODE */) {
+      range.setStart(el.firstChild!, offset);
+      range.setEnd(el.firstChild!, offset);
+  } else {
+      range.setStart(el!, 0);
+      range.setEnd(el!, 0);
+  }
 	}
 
 	return range;

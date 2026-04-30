8ec9fae3b29955501038ac3f1908f4fb50ec7849
Rui Neves <rui.neves@outsystems.com>
2026-04-30 15:46:03 +0100

    CR

--- src/vs/editor/browser/controller/mouseTarget.ts
+++ src/vs/editor/browser/controller/mouseTarget.ts
@@ -1172,9 +1172,9 @@ function shadowCaretRangeFromPoint(shadowRoot: ShadowRoot, x: number, y: number)
 		}
 
 		// Creates a range with the text node of the element and set the offset found
-		if (el.firstChild!.nodeType === 3 /* TEXT_NODE */) {
-  		range.setStart(el.firstChild!, offset);
-  		range.setEnd(el.firstChild!, offset);
+		if (el.firstChild!.nodeType ===  Node.TEXT_NODE) {
+			range.setStart(el.firstChild!, offset);
+			range.setEnd(el.firstChild!, offset);
 		} else {
 			range.setStart(el!, 0);
 			range.setEnd(el!, 0);
